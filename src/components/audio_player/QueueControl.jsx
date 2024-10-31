ee14c2121becc8e5b03d5fb275969d219207d3a3
peter <peter.l.nolan@gmail.com>
2024-10-31 15:01:11 -0400

    Add drag and drop library Dragula for queue control, patch vulnerabilities

--- src/components/audio_player/QueueControl.jsx
+++ src/components/audio_player/QueueControl.jsx
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux"
 import QueueItem from "./QueueItem";
 import { useState, useCallback} from "react";
 import './QueueControl.css'
+import dragula from "dragula";
 // import * as playlistActions from '../../store/playlist.js'
 
 
@@ -19,6 +20,16 @@ export default function QueueControl () {
             .map(idx => state.tracks[idx])
     });
 
+    const drake = dragula([document.getElementById("queue-list")], {
+        moves: function (el, container, handle) {
+            return handle.classList.contains('handle');
+        },
+        accepts: function(el, target, source, sibling) {
+            return true
+        },
+        direction: 'vertical',
+    })
+
     const toggleDisplay = useCallback((e) => {
         e.preventDefault();
         setDisplay(!display);
@@ -33,16 +44,18 @@ export default function QueueControl () {
     return (
         <div className="queue-control container">
             <button className="queue-control button" onClick={toggleDisplay} title="Playlist menu">
-                <i className="fa-solid fa-music"/>
+                <i className="fa-solid fa-list"/>
             </button>
             <div className={display ? "queue-control inner hidden" : "queue-control inner"}>
                 <div className="queue-control-header">
                     <span>Next Up</span>
                     <button onClick={handlePlaylistSave}>Save Playlist</button>
                 </div>
-                <ul className="queue">
-                    { tracks.map((track, i) => <li key={i}><QueueItem track={track}/></li>)}
-                </ul>
+                { tracks.length ? 
+                    (<ul className="queue" id="queue-list"> 
+                        { tracks.map((track, i) => <li key={i}><QueueItem track={track}/></li>) }
+                    </ul>)
+                    : false }
             </div>
         </div>
     )
--- src/components/audio_player/QueueItem.jsx
+++ src/components/audio_player/QueueItem.jsx
@@ -1,7 +1,12 @@
-export default function QueueItem({track}) {
 
+
+export default function QueueItem({track}) {
+    
     return (
         <div className="queue-item">
+            {/* <div className="handle"> */}
+                <i className="fa-solid fa-grip-vertical handle"></i>
+            {/* </div> */}
             <span className="title">{track.title}</span> 
             <span className="artist"> {track.artist.username}</span>
         </div>
