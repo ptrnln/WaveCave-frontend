13ff698f32b1562c06cf489a78d8078759fd589c
peter <peter.l.nolan@gmail.com>
2025-03-22 23:53:55 -0400

    minor styling change

--- src/components/audio_player/QueueControl.jsx
+++ src/components/audio_player/QueueControl.jsx
@@ -58,7 +58,6 @@ export default function QueueControl () {
     useEffect(() => {
         window.addEventListener('resize', forceUpdate)
         // window.addEventListener('drag', handleDragInterruption)
-        
         return () => {
             window.removeEventListener('resize', forceUpdate)
             // window.removeEventListener('drag', handleDragInterruption)
